911eef3910ee192f6626ff367fa2b4338f993a35
Alex Olar <qbeer666@q.krft>
2019-09-13 13:02:58 +0200

    gan added

--- gan-basics/train.py
+++ gan-basics/train.py
@@ -2,8 +2,12 @@
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 import tensorflow as tf
 tf.enable_eager_execution()
+
 from gan import GAN
 
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
@@ -12,11 +16,11 @@ train_images = train_images.reshape(train_images.shape[0], 28, 28,
 train_images = train_images / 255.  # Normalize the images to [0, 1]
 
 BUFFER_SIZE = 60000
-BATCH_SIZE = 256
+BATCH_SIZE = 1024
 
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(
     BUFFER_SIZE).batch(BATCH_SIZE)
 
 gan = GAN()
 
-gan.train(train_dataset, batch_size=BATCH_SIZE)
+gan.train(train_dataset, batch_size=BATCH_SIZE, epochs=400)
