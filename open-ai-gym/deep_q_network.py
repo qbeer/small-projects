953a1ca5e3d337f6289bdc697a0d8b3915c20157
Alex Olar <olaralex@phys-gs.elte.hu>
2019-11-30 09:55:46 +0100

    .

--- open-ai-gym/deep_q_network.py
+++ open-ai-gym/deep_q_network.py
@@ -4,14 +4,14 @@ import tensorflow as tf
 class DeepQNetwork(tf.keras.Model):
     def __init__(self, n_actions):
         super(DeepQNetwork, self).__init__()
-        self.conv1 = tf.keras.layers.Conv2D(16, (3, 3),
+        self.conv1 = tf.keras.layers.Conv2D(4, (3, 3),
                                             strides=(2, 2),
                                             activation='relu')
-        self.conv2 = tf.keras.layers.Conv2D(32, (3, 3),
+        self.conv2 = tf.keras.layers.Conv2D(8, (3, 3),
                                             strides=(2, 2),
                                             activation='relu')
         self.flatten = tf.keras.layers.Flatten()
-        self.dense1 = tf.keras.layers.Dense(64, activation='relu')
+        self.dense1 = tf.keras.layers.Dense(16, activation='relu')
         self.dense2 = tf.keras.layers.Dense(n_actions)
 
     def call(self, inputs):
