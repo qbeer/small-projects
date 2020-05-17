1baa268cd6004946ee24a503317c4402add97bc4
Alex Olar <olaralex666@gmail.com>
2020-05-17 21:01:35 +0200

    test it elsewhere

--- open-ai-gym/dqn.py
+++ open-ai-gym/dqn.py
@@ -1,9 +1,11 @@
 import os
-#os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import tensorflow as tf
+import timeit
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
+
 if gpus:
   try:
     # Currently, memory growth needs to be the same across GPUs
@@ -29,7 +31,7 @@ test_env = gym.make('Pong-v0')
 
 N_ACTIONS = env.action_space.n
 GAMMA = 0.99
-MAX_EPISODE_LENGTH = 500
+MAX_EPISODE_LENGTH = 300
 N_EPOSIDES = 10_000
 UPDATE_INTERVAL = 2_500
 REPLAY_MEMORY_SIZE = 125_000
@@ -50,7 +52,6 @@ def get_current_epsilon(n_th_step):
     else:
         return EPS_MAX - (EPS_MAX - EPS_MIN) * n_th_step / ANNEALATION_STEPS
 
-# actions are : NOPE, FIRE (new ball), RIGHT, LEFT
 q = DeepQNetwork(N_ACTIONS)
 q.build((None, IMG_HEIGHT, IMG_WIDTH, STACK_SIZE))
 
@@ -80,42 +81,29 @@ def preprocess_input(frames):
         observed[..., ind] += obs / 255.
     return observed.astype(np.float32)
 
-@tf.function(experimental_relax_shapes=True)
+@tf.function(input_signature=[
+    tf.TensorSpec(shape=(MINI_BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, STACK_SIZE), dtype=tf.float32),
+    tf.TensorSpec(shape=(MINI_BATCH_SIZE,), dtype=tf.uint8),
+    tf.TensorSpec(shape=(MINI_BATCH_SIZE,), dtype=tf.float32),
+    tf.TensorSpec(shape=(MINI_BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, STACK_SIZE), dtype=tf.float32),
+    tf.TensorSpec(shape=(MINI_BATCH_SIZE,), dtype=tf.bool),
+])
 def perform_gradient_step_on_q_net(STATES, ACTIONS, REWARDS, NEXT_STATES, IS_TERMINALS):
-    targets = []
-    states = []
-    actions = []
-    
+ 
     loss_fn = tf.keras.losses.Huber()
     
-    for ind in range(MINI_BATCH_SIZE):
-        state, action, reward, next_state, is_terminal = STATES[ind], ACTIONS[ind],\
-            REWARDS[ind], NEXT_STATES[ind], IS_TERMINALS[ind]
-        
-        if not is_terminal:
-            bootstrap = GAMMA * tf.reduce_max(q_target(tf.expand_dims(next_state, axis=0)))
-            reward += bootstrap
-            
-        targets.append(reward)
-        states.append(state)
-        actions.append(tf.one_hot(action, depth=N_ACTIONS))
-    
-    states = tf.stack(states)
-    
-    actions = tf.stack(actions)
-    
-    targets = tf.stack(targets)
+    targets = REWARDS + tf.multiply(tf.cast(IS_TERMINALS, tf.float32),
+                                    GAMMA * tf.reduce_max(q_target(NEXT_STATES), axis=1))
+    actions = tf.one_hot(ACTIONS, depth=N_ACTIONS)
            
     with tf.GradientTape() as tape:
-        selected_states = tf.multiply(q(states), actions)
-        selected_states = tf.reduce_max(selected_states, axis=1)
-        objective = loss_fn(targets, selected_states)
+        selected_action_values = tf.multiply(q(STATES), actions)
+        selected_action_values = tf.reduce_max(selected_action_values, axis=1)
+        objective = loss_fn(targets, selected_action_values)
         
     grads = tape.gradient(objective, q.trainable_weights)
     
     OPTMIZER.apply_gradients(zip(grads, q.trainable_weights))
-    
-    return targets, selected_states, actions
 
 def test_agent():
     
@@ -153,7 +141,6 @@ def test_agent():
 n_th_iteration = 0
 
 for ep in range(N_EPOSIDES):
-    
     initial_frame = env.reset() # initial observation
     frames = collections.deque(maxlen=STACK_SIZE)
     for _ in range(STACK_SIZE):
@@ -162,6 +149,8 @@ for ep in range(N_EPOSIDES):
     state = preprocess_input(frames)
     
     total_reward = 0
+    
+    start = timeit.timeit()
     
     for timestep in range(MAX_EPISODE_LENGTH):
         
@@ -186,41 +175,33 @@ for ep in range(N_EPOSIDES):
         
         if n_th_iteration > MIN_EXPERIENCE_STEPS:
             samples = memory.sample_experiences(MINI_BATCH_SIZE)
-            STATES = tf.convert_to_tensor([sample[0] for sample in samples])
-            ACTIONS = tf.convert_to_tensor([sample[1] for sample in samples])
-            REWARDS = tf.convert_to_tensor([sample[2] for sample in samples])
-            NEXT_STATES = tf.convert_to_tensor([sample[3] for sample in samples])
-            IS_TERMINALS = tf.convert_to_tensor([sample[4] for sample in samples])
+            STATES = tf.convert_to_tensor([sample[0] for sample in samples], dtype=tf.float32)
+            ACTIONS = tf.convert_to_tensor([sample[1] for sample in samples], dtype=tf.uint8)
+            REWARDS = tf.convert_to_tensor([sample[2] for sample in samples], dtype=tf.float32)
+            NEXT_STATES = tf.convert_to_tensor([sample[3] for sample in samples], dtype=tf.float32)
+            IS_TERMINALS = tf.convert_to_tensor([sample[4] for sample in samples], dtype=tf.bool)
             
-            targets, selected_states, actions = perform_gradient_step_on_q_net(STATES,
-                                                                               ACTIONS,
-                                                                               REWARDS,
-                                                                               NEXT_STATES,
-                                                                               IS_TERMINALS)
+            perform_gradient_step_on_q_net(STATES,
+                                            ACTIONS,
+                                            REWARDS,
+                                            NEXT_STATES,
+                                            IS_TERMINALS)
             
             if (n_th_iteration - MIN_EXPERIENCE_STEPS) % UPDATE_INTERVAL == 0:
                 logging.info('Iteration : %d | Updating target weights...' % n_th_iteration)
+                
                 q.save_weights('chkpt/q.h5')
-                
-                logging.info('Loss : %.5f' % np.mean((targets - selected_states)**2))
-                
-                for t, s_s in zip(targets, selected_states):
-                    logging.info(f'target : {t}, predicted_target : {s_s}')
-                
                 q_target.set_weights(q.get_weights())
                 
                 test_agent()
-                
-                samples = memory.sample_experiences(16)
-                
-                for samp in samples:
-                    logging.info(f'action : {samp[1]}, reward : {samp[2]}, terminal : {samp[-1]}')
             
         if terminal:
             total_reward -= 1
             break
+        
+    end = timeit.timeit()
+    print(start - end)
+       
+    logging.info(f'Iteration : {n_th_iteration} | Episode : {ep + 1} | Total reward : {total_reward}, episode length : {timestep}, current eps : {current_eps}')
     
-    if n_th_iteration % 3000 == 0:        
-        logging.info(f'Iteration : {n_th_iteration} | Episode : {ep + 1} | Total reward : {total_reward}, episode length : {timestep}, current eps : {current_eps}')      
-
 env.close()
